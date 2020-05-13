ac67de905096f225b7cca7e2863f3d2970ea5709
Kaveh <kavehfrounchi@gmail.com>
2020-05-13 15:50:41 +0430

    Passport Authentication

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const {check, validationResult} = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 //Bring in User Model
 let User = require('../models/user');
@@ -19,7 +20,7 @@ router.post('/register', [
     check('username', 'Username is required').not().isEmpty(),
     check('password', 'Password is required').not().isEmpty(),
     check('password2', 'Passwords do not match').custom((value, {req}) => (value === req.body.password)),
-],function (req, res) {
+], function (req, res) {
     const name = req.body.name;
     const email = req.body.email;
     const username = req.body.username;
@@ -27,26 +28,26 @@ router.post('/register', [
     const password2 = req.body.password2;
 
     let errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         res.render('register', {
-            errors:errors.array()
+            errors: errors.array()
         });
     } else {
         let newUser = new User({
-            name:name,
-            email:email,
-            username:username,
-            password:password
+            name: name,
+            email: email,
+            username: username,
+            password: password
         });
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(newUser.password, salt, function(err, hash) {
-                if(err){
+        bcrypt.genSalt(10, function (err, salt) {
+            bcrypt.hash(newUser.password, salt, function (err, hash) {
+                if (err) {
                     console.log(err);
                 }
                 newUser.password = hash;
-                newUser.save(function(err){
+                newUser.save(function (err) {
 
-                    if(err){
+                    if (err) {
                         console.log(err);
                         return;
                     } else {
@@ -59,9 +60,23 @@ router.post('/register', [
         })
     }
 });
-
+//Login Form
 router.get('/login', function (req, res) {
     res.render('login')
 });
+//Login Process
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
 
+//Logout
+router.get('/logout', function (req, res) {
+    req.logout();
+    req.flash('success', 'You are logged out');
+    res.redirect('/users/login');
+});
 module.exports = router;
